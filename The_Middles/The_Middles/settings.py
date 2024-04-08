d62f54b891a46addb777af5e47a8551d5a24d93d
sambt22022517 <22022517@vnu.edu.vn>
2024-04-08 13:48:25 +0700

    fix: Sửa submit: input -> button

--- The_Middles/The_Middles/settings.py
+++ The_Middles/The_Middles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!y&%5&7$%&_x0epty$es!pmy)d33hq#aikcxnk500v^a_b4ox6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
