e4e8bbe7962b8384aa7025599ad2acf97bfa2448
hoandkjxhbfhk <hoanoc321@hoanoc321gmail.com>
2024-04-10 11:42:23 +0700

    v

--- web/shop/urls.py
+++ web/shop/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("search/", views.search, name="Search"),
     path("productview/", views.productView, name="ProductView"),
     path("checkout/", views.checkout, name="Checkout"),
+    path("products/<int:myid>", views.productView, name="ProductView"),
 ]
