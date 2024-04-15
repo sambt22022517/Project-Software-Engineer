b64c813d2c7636c7b7432eef43d2f8ba959ed750
sambt22022517 <22022517@vnu.edu.vn>
2024-04-15 20:09:01 +0700

    initial: Khởi tạo home

---
+++ The_Middles/home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from models import Product
+
+def home(request):
+    products = Product.objects.all().values()
+    context = {
+        'products': products,
+    }
+    return render(request, 'home.html', context=context)
