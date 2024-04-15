ca68ec739a69542c3d03447e5dd2dee4f790356b
sambt22022517 <22022517@vnu.edu.vn>
2024-04-15 22:02:58 +0700

    demo: Làm một cái demo nhỏ cho home

--- The_Middles/The_Middles/settings.py
+++ The_Middles/The_Middles/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'The_Middles.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'THE_MIDDLES': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
