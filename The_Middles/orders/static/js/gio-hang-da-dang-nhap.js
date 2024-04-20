32148ebee69ca2874fdae09671d4775a4121783f
NguyenManh <22022521@vnu.edu.vn>
2024-04-20 12:40:54 +0700

    add: thêm thông báo chưa chọn sản phẩm nào

--- The_Middles/orders/static/js/gio-hang-da-dang-nhap.js
+++ The_Middles/orders/static/js/gio-hang-da-dang-nhap.js
@@ -1,9 +1,11 @@
 console.log("đã gọi file javascript");
 const select_all_product = document.querySelector(".gio-hang-da-dang-nhap-checkbox"); // chỉ có 1 phần tử
 const select_each_product = document.querySelectorAll(".gio-hang-da-dang-nhap-checkbox-chon-sp"); //có thể có nhiều phần tử
+const click_button_checkout = document.querySelector(".gio-hang-da-dang-nhap-button-mua-hang"); //chỉ có 1 phần tử 
+const not_selected_product = document.querySelector(".chua-chon-san-pham-root-class-name2");
+const close_notice_not_selected_product = document.querySelector(".chua-chon-san-pham-button");
 
 const click_button_search = document.getElementsByClassName("gio-hang-da-dang-nhap-button button"); //chỉ có 1 phần tử
-const click_button_checkout = document.getElementsByClassName("gio-hang-da-dang-nhap-button-mua-hang button"); //chỉ có 1 phần tử 
 const click_button_increase_quantity = document.getElementsByClassName("gio-hang-da-dang-nhap-button-tang-sl button"); //có thể có nhiều phần tử
 const click_button_decrease_quantity = document.getElementsByClassName("gio-hang-da-dang-nhap-button-giam-sl button"); //có thể có nhiều phần tử
 const input_quantity = document.getElementsByClassName("gio-hang-da-dang-nhap-input-sl input"); // có thể có nhiều phần tử
@@ -127,3 +129,25 @@ select_all_product.addEventListener('change', function(event){
         })
     }
 });
+
+// Trước khi mua hàng, phải kiểm tra xem có sản phẩm nào được chọn chưa
+click_button_checkout.addEventListener('click', function(event){
+    var count_checked = 0;
+    select_each_product.forEach(element => {
+        if (element.checked){
+            count_checked ++;
+        }
+    })
+    if (count_checked === 0){
+        console.log("chưa có sản phẩm nào được chọn");
+        not_selected_product.style.display = "flex";
+    }else{
+
+    }
+})
+
+// đóng thông báo chưa chọn sp
+close_notice_not_selected_product.addEventListener('click', function(event){
+    not_selected_product.style.display = "none";
+});
+
