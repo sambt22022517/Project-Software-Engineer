739ab5cbc97182d73bb035fdb2f11cbc463420ed
Bùi Tiến Sâm 22022517 <120190847+sambt22022517@users.noreply.github.com>
2024-05-18 15:15:41 +0000

    fix: Xoá phần code lỗi

--- The_Middles/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('danh-muc-san-pham/', views.category, name='category'),
-    path('danh-muc-san-pham/<type>', views.category, name='category'),
-]
--- The_Middles/orders/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Order, OrderProduct, Payment
-
-
-admin.site.register(Order)
-admin.site.register(Payment)
-admin.site.register(OrderProduct)
