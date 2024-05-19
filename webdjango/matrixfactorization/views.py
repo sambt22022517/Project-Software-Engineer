211f0770cedac86c31f6098c282300ff806dcd23
hoandkjxhbfhk <120200615+hoandkjxhbfhk@users.noreply.github.com>
2024-05-19 11:08:20 +0000

    add views

--- webdjango/matrixfactorization/views.py
+++ webdjango/matrixfactorization/views.py
@@ -37,3 +37,41 @@ def recommend(request):
     preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(pred_idxs_sorted)])
     movie_list = list(Product.objects.filter(id__in=pred_idxs_sorted, ).order_by(preserved)[:10])
     return render(request, 'web/recommend.html', {'movie_list': movie_list})
+
+def recom(request, k=8):
+    user_name = request.user.username
+    all_users = User.objects.all()
+    data_for_uuCF = []
+    product_ids = []
+    for product_id, name, rating in all_reviews:
+        user_id = user_id_dict.get(name)
+        data_for_uuCF.append([user_id, product_id, rating])  # Adjust column order if needed
+        product_ids.append(product_id)  # Store product IDs for later use
+    model = uuCF(data_for_uuCF, k=k)
+    model.fit()
+
+    predictions = {}
+    for product_id in product_ids:
+        predicted_rating = model.pred(user_id_dict.get(user_name), product_id)
+        predictions[product_id] = predicted_rating
+
+    # Filter out already rated products
+    user_reviews = Review.objects.filter(user_name=user_name).values_list("product_id", flat=True)
+    already_rated_product_ids = set(user_reviews)
+
+    # Sort and select top-k recommendations
+    recommendations = []
+    for product_id, predicted_rating in predictions.items():
+        if product_id not in already_rated_product_ids:
+            recommendations.append((product_id, predicted_rating))
+    recommendations.sort(key=lambda x: x[1], reverse=True)  # Sort by predicted rating
+    top_k_recommendations = recommendations[:k]
+
+    # Retrieve recommended products
+    recommended_product_ids = [rec[0] for rec in top_k_recommendations]
+    recommended_products = Product.objects.filter(id__in=recommended_product_ids)
+    context = {
+        "movie_list": recommended_products,
+        "user_id": request.user.id,
+    }
+    return render(request, "recommend.html", context)
--- webdjango/order/views.py
+++ webdjango/order/views.py
@@ -1,99 +1,101 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
-
 from django.forms import ModelForm
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .models import ShopCart, ShopCartForm, OrderForm, Order, OrderDetail
+from .models import Order, OrderDetail, OrderForm, ShopCart, ShopCartForm
 
 
 # Create your views here.
-@login_required(login_url='/login')
+@login_required(login_url="/login")
 def index(request):
     current_user = request.user
     orders = Order.objects.all().filter(user_id=current_user.id)
 
-    context = {'page': 'orders',
-               'orders': orders,
-               }
-    return render(request, 'order_list.html', context)
+    context = {
+        "page": "orders",
+        "orders": orders,
+    }
+    return render(request, "order_list.html", context)
 
 
-@login_required(login_url='/login')
+@login_required(login_url="/login")
 def shop_cart_list(request):
     current_user = request.user
     shopcart = ShopCart.objects.all().filter(user_id=current_user.id)
     carttotal = 0
     for rs in shopcart:
         carttotal += rs.quantity * rs.product.discount_price
-    carttax = (14/100)*carttotal
-    cartwithtax= carttotal+carttax
+    carttax = round((10 / 100) * carttotal, 1)
 
-    context = {'page': 'cart',
-               'shopcart': shopcart,
-               'carttotal': carttotal,
-               'carttax':carttax,
-               'cartwithtax':cartwithtax
-               }
-    return render(request, 'shop_cart_list.html', context)
+    cartwithtax = carttotal + carttax
+
+    context = {
+        "page": "cart",
+        "shopcart": shopcart,
+        "carttotal": carttotal,
+        "carttax": carttax,
+        "cartwithtax": cartwithtax,
+    }
+    return render(request, "shop_cart_list.html", context)
 
 
-@login_required(login_url='/login')
+@login_required(login_url="/login")
 def shop_cart_add(request, product_id):
     # if this is a POST request we need to process the form data
-    url = request.META.get('HTTP_REFERER')  #
+    url = request.META.get("HTTP_REFERER")  #
     # return HttpResponse(url)
     form = ShopCartForm(request.POST or None)
-    if request.method == 'POST':
+    if request.method == "POST":
         # check whether it's valid:
         if form.is_valid():
             current_user = request.user  # Get User id
-            quantity = form.cleaned_data['quantity']  # get product quantity from form
+            quantity = form.cleaned_data["quantity"]  # get product quantity from form
 
             # Checking product in ShopCart
             try:
                 q1 = ShopCart.objects.get(user_id=current_user.id, product_id=product_id)
             except ShopCart.DoesNotExist:
                 q1 = None
-            if q1 != None:  # Update  quantity to exist product quantity
+            if q1 is not None:  # Update  quantity to exist product quantity
                 q1.quantity = q1.quantity + quantity
                 q1.save()
             else:  # Add new item to shop cart
                 data = ShopCart(user_id=current_user.id, product_id=product_id, quantity=quantity)
                 data.save()
-            request.session['cart_items'] = ShopCart.objects.filter(
-                user_id=current_user.id).count()  # Count item in shop cart
+            request.session["cart_items"] = ShopCart.objects.filter(
+                user_id=current_user.id
+            ).count()  # Count item in shop cart
             messages.success(request, "Product added to cart.. ")
             return HttpResponseRedirect(url)
 
-    return HttpResponseRedirect(reverse('shop:product_detail', args=[product_id]))
+    return HttpResponseRedirect(reverse("shop:product_detail", args=[product_id]))
 
 
-@login_required(login_url='/login')
+@login_required(login_url="/login")
 def shop_cart_delete(request, id):
-    url = request.META.get('HTTP_REFERER')  # Bir önceki adresi alır
+    url = request.META.get("HTTP_REFERER")  
     ShopCart.objects.filter(id=id).delete()
     messages.success(request, "Product deleted from  cart.. ")
     return HttpResponseRedirect(url)
 
 
-@login_required(login_url='/login')
+@login_required(login_url="/login")
 def shop_cart_checkout(request):
     current_user = request.user
     shopcart = ShopCart.objects.all().filter(user_id=current_user.id)
     carttotal = 0
-    carttax =0
-    cartwithtax =0
+    carttax = 0
+    cartwithtax = 0
     for rs in shopcart:
         carttotal += rs.quantity * rs.product.discount_price
-        carttax = (14 / 100) * carttotal
+        carttax = (10 / 100) * carttotal
         cartwithtax = carttotal + carttax
 
     form = OrderForm(request.POST or None)
-    if request.method == 'POST':
+    if request.method == "POST":
         # check whether it's valid:
         if form.is_valid():
 
@@ -101,12 +103,12 @@ def shop_cart_checkout(request):
             # If payment accepted continue else send payment error to checkout page
 
             data = Order()
-            data.name = form.cleaned_data['name']  # get product quantity from form
-            data.surname = form.cleaned_data['surname']
-            data.address = form.cleaned_data['address']
-            data.city = form.cleaned_data['city']
-            data.phone = form.cleaned_data['phone']
-            data.to = form.cleaned_data['name']
+            data.name = form.cleaned_data["name"]  # get product quantity from form
+            data.surname = form.cleaned_data["surname"]
+            data.address = form.cleaned_data["address"]
+            data.city = form.cleaned_data["city"]
+            data.phone = form.cleaned_data["phone"]
+            data.to = form.cleaned_data["name"]
             data.user_id = current_user.id
             data.total = carttotal
             data.save()
@@ -124,26 +126,17 @@ def shop_cart_checkout(request):
                 #  Reduce product Amount  (quantity)
 
             ShopCart.objects.filter(user_id=current_user.id).delete()  # Clear & Delete shopcart
-            request.session['cart_items'] = 0
-            messages.success(request, "Order has been completed. Thank You ")
+            request.session["cart_items"] = 0
+            messages.success(request, "Đơn hàng đã được ghi nhận. Xin cảm ơn ")
             return HttpResponseRedirect("/order")
 
-    context = {'page': 'checkout',
-               'shopcart': shopcart,
-               'carttax':carttax,
-               'cartwithtax':cartwithtax,
-               'carttotal': carttotal,
-               }
-    return render(request, 'shop_cart_checkout.html', context)
+    context = {
+        "page": "checkout",
+        "shopcart": shopcart,
+        "carttax": carttax,
+        "cartwithtax": cartwithtax,
+        "carttotal": carttotal,
+    }
+    return render(request, "shop_cart_checkout.html", context)
 
 
-@login_required(login_url='/login')
-def order_detail(request, id):
-    order = Order.objects.get(pk=id)
-    items = OrderDetail.objects.all().filter(order=id)
-
-    context = {'page': 'detail',
-               'order': order,
-               'items': items,
-               }
-    return render(request, 'order_detail.html', context)
